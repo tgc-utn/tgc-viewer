8e4d9583b4bb1f70605fcabc676480a98a04e2e8
leonematias@gmail.com <leonematias@gmail.com@274b6dbb-dcc3-7c66-b4d0-09bc4b5f3247>
2013-10-08 20:25:32 +0000

    # Completando opciones de Object Browser del Mesh Creator

---
+++ trunk/TgcViewer/Examples/MeshCreator/ObjectBrowser.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Examples.MeshCreator
+{
+    partial class ObjectBrowser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.buttonHide = new System.Windows.Forms.Button();
+            this.buttonShow = new System.Windows.Forms.Button();
+            this.buttonNewLayer = new System.Windows.Forms.Button();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.treeViewObjects = new System.Windows.Forms.TreeView();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonMove = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.buttonMove);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonDelete);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonHide);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonShow);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonNewLayer);
+            this.splitContainer1.Panel1.Controls.Add(this.textBoxSearch);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonSearch);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.treeViewObjects);
+            this.splitContainer1.Size = new System.Drawing.Size(407, 565);
+            this.splitContainer1.SplitterDistance = 69;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // buttonHide
+            // 
+            this.buttonHide.Location = new System.Drawing.Point(128, 43);
+            this.buttonHide.Name = "buttonHide";
+            this.buttonHide.Size = new System.Drawing.Size(42, 23);
+            this.buttonHide.TabIndex = 4;
+            this.buttonHide.Text = "Hide";
+            this.buttonHide.UseVisualStyleBackColor = true;
+            this.buttonHide.Click += new System.EventHandler(this.buttonHide_Click);
+            // 
+            // buttonShow
+            // 
+            this.buttonShow.Location = new System.Drawing.Point(86, 43);
+            this.buttonShow.Name = "buttonShow";
+            this.buttonShow.Size = new System.Drawing.Size(42, 23);
+            this.buttonShow.TabIndex = 3;
+            this.buttonShow.Text = "Show";
+            this.buttonShow.UseVisualStyleBackColor = true;
+            this.buttonShow.Click += new System.EventHandler(this.buttonShow_Click);
+            // 
+            // buttonNewLayer
+            // 
+            this.buttonNewLayer.Location = new System.Drawing.Point(3, 43);
+            this.buttonNewLayer.Name = "buttonNewLayer";
+            this.buttonNewLayer.Size = new System.Drawing.Size(37, 23);
+            this.buttonNewLayer.TabIndex = 2;
+            this.buttonNewLayer.Text = "New";
+            this.buttonNewLayer.UseVisualStyleBackColor = true;
+            this.buttonNewLayer.Click += new System.EventHandler(this.buttonNewLayer_Click);
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Location = new System.Drawing.Point(3, 6);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(262, 20);
+            this.textBoxSearch.TabIndex = 1;
+            this.textBoxSearch.TextChanged += new System.EventHandler(this.textBoxSearch_TextChanged);
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(271, 4);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 0;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // treeViewObjects
+            // 
+            this.treeViewObjects.CheckBoxes = true;
+            this.treeViewObjects.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeViewObjects.Location = new System.Drawing.Point(0, 0);
+            this.treeViewObjects.Name = "treeViewObjects";
+            this.treeViewObjects.Size = new System.Drawing.Size(407, 492);
+            this.treeViewObjects.TabIndex = 0;
+            this.treeViewObjects.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.treeViewObjects_AfterCheck);
+            this.treeViewObjects.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeViewObjects_AfterSelect);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(40, 43);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(47, 23);
+            this.buttonDelete.TabIndex = 5;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonMove
+            // 
+            this.buttonMove.Location = new System.Drawing.Point(169, 43);
+            this.buttonMove.Name = "buttonMove";
+            this.buttonMove.Size = new System.Drawing.Size(47, 23);
+            this.buttonMove.TabIndex = 6;
+            this.buttonMove.Text = "Move";
+            this.buttonMove.UseVisualStyleBackColor = true;
+            this.buttonMove.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // ObjectBrowser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 565);
+            this.Controls.Add(this.splitContainer1);
+            this.MinimizeBox = false;
+            this.Name = "ObjectBrowser";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Object browser";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ObjectBrowser_FormClosing);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button buttonNewLayer;
+        private System.Windows.Forms.TreeView treeViewObjects;
+        private System.Windows.Forms.Button buttonShow;
+        private System.Windows.Forms.Button buttonHide;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonMove;
+    }
+}
