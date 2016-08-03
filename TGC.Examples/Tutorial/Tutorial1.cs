4826a7283bae5fe81644a2940ea060957ce1020a
René Juan Rico Mendoza <rejurime@gmail.com>
2016-08-03 16:37:03 -0300

    Tutoriales done #48

--- TGC.Examples/Tutorial/Tutorial1.cs
+++ TGC.Examples/Tutorial/Tutorial1.cs
@@ -5,27 +5,43 @@ using TGC.Core.Geometry;
 using TGC.Core.UserControls;
 using TGC.Core.UserControls.Modifier;
 using TGC.Examples.Example;
+using TGC.Core.Textures;
+using TGC.Core.Utils;
+using Microsoft.DirectX.DirectInput;
 
 namespace TGC.Examples.Tutorial
 {
-    /// <summary>
-    ///     Tutorial 1:
-    ///     Unidades Involucradas:
-    ///     # Unidad 3 - Conceptos Básicos de 3D - Mesh
-    ///     Muestra como crear una caja 3D de color y como mostrarla por pantalla.
-    ///     Autor: Matías Leone
-    /// </summary>
-    public class Tutorial1 : TGCExampleViewer
+	/// <summary>
+	///     Tutorial 1:
+	///     Unidades Involucradas:
+	///     # Unidad 3 - Conceptos Básicos de 3D - Mesh
+	/// 	# Unidad 4 - Texturas e iluminacion - Texturas
+	///     Muestra como crear una caja 3D de color y como mostrarla por pantalla.
+	/// 	Muestra como crear una caja 3D con una imagen 2D como textura para darle color.
+	/// 	Muestra como crear una caja 3D con textura que se traslada y rota en cada cuadro.
+	/// 	Muestra como crear una caja 3D que se mueve cuando las flechas del teclado.
+	///     Autor: Matías Leone
+	/// </summary>
+	public class Tutorial1 : TGCExampleViewer
     {
-        //Variable para caja 3D
-        private TgcBox box;
+		//Constantes para velocidades de movimiento
+		private const float ROTATION_SPEED = 1f;
+		private const float MOVEMENT_SPEED = 5f;
+
+        //Variables para las cajas 3D
+        private TgcBox box1;
+		private TgcBox box2;
+		private TgcBox box3;
+
+		//Variable direccion de movimiento
+		private float currentMoveDir = 1f;
 
         public Tutorial1(string mediaDir, string shadersDir, TgcUserVars userVars, TgcModifiers modifiers)
             : base(mediaDir, shadersDir, userVars, modifiers)
         {
             Category = "Tutorial";
             Name = "Tutorial 1";
-            Description = "Muestra como crear una caja 3D de color y como mostrarla por pantalla.";
+            Description = "Ejemplos de Creación de Cajas 3D con color, con imagen 2D como Textura, con traslación y con rotación";
         }
 
         /// <summary>
@@ -34,29 +50,65 @@ namespace TGC.Examples.Tutorial
         /// </summary>
         public override void Init()
         {
-            //Creamos una caja 3D de color rojo, ubicada en el origen y lado 10
-            var center = new Vector3(0, 0, 0);
+			//Todos los recursos que se van a necesitar (objetos 3D, meshes, texturas, etc) se deben cargar en el metodo init().
+			//Crearlos cada vez en el metodo render() es un error grave. Destruye la performance y suele provocar memory leaks.
+
+			//Creamos una caja 3D de color rojo, ubicada en el origen y lado 10
+			var center = new Vector3(0, 0, 0);
             var size = new Vector3(10, 10, 10);
             var color = Color.Red;
-            box = TgcBox.fromSize(center, size, color);
+            box1 = TgcBox.fromSize(size, color);
+			box1.Transform = Matrix.Translation(center);
 
-            //Todos los recursos que se van a necesitar (objetos 3D, meshes, texturas, etc) se deben cargar en el metodo init().
-            //Crearlos cada vez en el metodo render() es un error grave. Destruye la performance y suele provocar memory leaks.
+			//Cargamos una textura una textura es una imágen 2D que puede dibujarse arriba de un polígono 3D para darle color.
+			//Es muy útil para generar efectos de relieves y superficies.
+			//Puede ser cualquier imágen 2D (jpg, png, gif, etc.) y puede ser editada con cualquier editor
+			//normal (photoshop, paint, descargada de goole images, etc).
+			//El framework viene con un montón de texturas incluidas y organizadas en categorias (texturas de
+			//madera, cemento, ladrillo, pasto, etc). Se encuentran en la carpeta del framework: Media\MeshCreator\Textures
+			//Podemos acceder al path de la carpeta "Media" utilizando la variable "this.MediaDir".
+			//Esto evita que tengamos que hardcodear el path de instalación del framework.
+			var texture = TgcTexture.createTexture(MediaDir + "MeshCreator\\Textures\\Madera\\cajaMadera3.jpg");
 
-            //Ubicar la camara del framework mirando al centro de este objeto.
-            //La camara por default del framework es RotCamera, cuyo comportamiento es
-            //centrarse sobre un objeto y permitir rotar y hacer zoom con el mouse.
-            //Con clic izquierdo del mouse se rota la cámara, con clic derecho se traslada y con la rueda
-            //del mouse se hace zoom.
-            //Otras cámaras disponibles son: FpsCamera (1ra persona) y ThirdPersonCamera (3ra persona).
-            Camara = new TgcRotationalCamera(box.BoundingBox.calculateBoxCenter(),
-                box.BoundingBox.calculateBoxRadius() * 2, Input);
+			//Creamos una caja 3D ubicada en (10, 0, 0) y la textura como color.
+			center = new Vector3(15, 0, 0);
+			box2 = TgcBox.fromSize(size, texture);
+			box2.Transform = Matrix.Translation(center);
+
+			//Creamos una caja 3D con textura
+			center = new Vector3(-15, 0, 0);
+			texture = TgcTexture.createTexture(MediaDir + "MeshCreator\\Textures\\Metal\\cajaMetal.jpg");
+			box3 = TgcBox.fromSize(center, size, texture);
+			box3.AutoTransformEnable = true;
+
+			//Ubicar la camara del framework mirando al centro de este objeto.
+			//La camara por default del framework es RotCamera, cuyo comportamiento es
+			//centrarse sobre un objeto y permitir rotar y hacer zoom con el mouse.
+			//Con clic izquierdo del mouse se rota la cámara, con el derecho se traslada y con la rueda se hace zoom.
+			//Otras cámaras disponibles (a modo de ejemplo) son: FpsCamera (1ra persona) y ThirdPersonCamera (3ra persona).
+			Camara = new TgcRotationalCamera(box1.BoundingBox.calculateBoxCenter(),
+                box1.BoundingBox.calculateBoxRadius() * 5, Input);
         }
 
         public override void Update()
         {
             PreUpdate();
-        }
+
+			//En cada cuadro de render rotamos la caja con cierta velocidad (en radianes)
+			//Siempre tenemos que multiplicar las velocidades por el elapsedTime.
+			//De esta forma la velocidad de rotacion es independiente de la potencia del CPU.
+			//Sino en computadoras con CPU más rápido la caja giraría mas rápido que en computadoras mas lentas.
+			box3.rotateY(ROTATION_SPEED * ElapsedTime);
+
+			//Aplicamos una traslación en Y. Hacemos que la caja se mueva en forma intermitente en el intervalo [0, 3] de Y. 
+			//Cuando llega a uno de los límites del intervalo invertimos la dirección del movimiento.
+			//Tambien tenemos que multiplicar la velocidad por el elapsedTime
+			box3.move(0, MOVEMENT_SPEED * currentMoveDir * ElapsedTime, 0);
+			if (FastMath.Abs(box3.Position.Y) > 3f)
+			{
+				currentMoveDir *= -1;
+			}
+		}
 
         /// <summary>
         ///     Método que se invoca todo el tiempo. Es el render-loop de una aplicación gráfica.
@@ -70,10 +122,11 @@ namespace TGC.Examples.Tutorial
         {
             //Iniciamoss la escena
             PreRender();
-            //Render de la super clase
 
-            //Dibujar la caja en pantalla
-            box.render();
+            //Dibujar las cajas en pantalla
+            box1.render();
+			box2.render();
+			box3.render();
 
             //Finalizamos el renderizado de la escena
             PostRender();
@@ -85,10 +138,12 @@ namespace TGC.Examples.Tutorial
         /// </summary>
         public override void Dispose()
         {
-            //Liberar memoria de la caja 3D.
+            //Liberar memoria de las cajas 3D.
             //Por mas que estamos en C# con Garbage Collector igual hay que liberar la memoria de los recursos gráficos.
             //Porque están utilizando memoria de la placa de video (y ahí no hay Garbage Collector).
-            box.dispose();
+            box1.dispose();
+			box2.dispose();
+			box3.dispose();
         }
     }
 }
--- TGC.Examples/Tutorial/Tutorial3.cs
+++ TGC.Examples/Tutorial/Tutorial3.cs
@@ -1,86 +1,159 @@
 using Microsoft.DirectX;
+using Microsoft.DirectX.DirectInput;
 using TGC.Core.Camara;
-using TGC.Core.Geometry;
-using TGC.Core.Textures;
+using TGC.Core.Collision;
+using TGC.Core.SceneLoader;
 using TGC.Core.UserControls;
 using TGC.Core.UserControls.Modifier;
-using TGC.Core.Utils;
 using TGC.Examples.Example;
 
 namespace TGC.Examples.Tutorial
 {
-    /// <summary>
-    ///     Tutorial 3:
-    ///     Unidades Involucradas:
-    ///     # Unidad 3 - Conceptos B·sicos de 3D - Transformaciones
-    ///     Muestra como crear una caja 3D con textura que se traslada y rota en cada cuadro.
-    ///     Autor: MatÌas Leone
-    /// </summary>
-    public class Tutorial3 : TGCExampleViewer
+	/// <summary>
+	///     Tutorial 3:
+	///     Unidades Involucradas:
+	///     # Unidad 3 - Conceptos Basicos de 3D - Mesh
+	/// 	# Unidad 6 - Detecci√≥n de Colisiones - Bounding Box.
+	///     Muestra como cargar una escena 3D y como mover un modelo dentra de ella con el teclado evitando chocar con el resto de los objetos.
+	///     Autor: Matias Leone
+	/// </summary>
+	public class Tutorial3 : TGCExampleViewer
     {
-        //Constantes para velocidades de movimiento
-        private const float ROTATION_SPEED = 1f;
-
-        private const float MOVEMENT_SPEED = 5f;
-        private TgcBox box;
-
-        //Variable direccion de movimiento
-        private float currentMoveDir = 1f;
+        private const float MOVEMENT_SPEED = 200f;
+        private TgcThirdPersonCamera camaraInterna;
+        private TgcMesh mainMesh;
+        private TgcScene scene;
 
         public Tutorial3(string mediaDir, string shadersDir, TgcUserVars userVars, TgcModifiers modifiers)
             : base(mediaDir, shadersDir, userVars, modifiers)
         {
             Category = "Tutorial";
             Name = "Tutorial 3";
-            Description = "Muestra como crear una caja 3D con textura que se traslada y rota en cada cuadro.";
+            Description = "Muestra como cargar una escena 3D y como mover un modelo dentra de ella con el teclado evitando chocar con el resto de los objetos.";
         }
 
         public override void Init()
         {
-            //Creamos una caja 3D con textura
-            var center = new Vector3(0, -3, 0);
-            var size = new Vector3(5, 5, 5);
-            var texture = TgcTexture.createTexture(MediaDir + "MeshCreator\\Textures\\Metal\\cajaMetal.jpg");
-            box = TgcBox.fromSize(center, size, texture);
+            //En este ejemplo primero cargamos una escena 3D entera.
+            var loader = new TgcSceneLoader();
+            scene = loader.loadSceneFromFile(MediaDir + "MeshCreator\\Scenes\\Ciudad\\Ciudad-TgcScene.xml");
 
-            //Hacemos que la c·mara estÈ centrada sobre el box.
-            Camara = new TgcRotationalCamera(box.BoundingBox.calculateBoxCenter(),
-                box.BoundingBox.calculateBoxRadius() * 2, Input);
+            //Luego cargamos otro modelo aparte que va a hacer el objeto que controlamos con el teclado
+            var scene2 =
+                loader.loadSceneFromFile(MediaDir + "MeshCreator\\Meshes\\Vehiculos\\Hummer\\Hummer-TgcScene.xml");
+
+            //Solo nos interesa el primer modelo de esta escena (tiene solo uno)
+            mainMesh = scene2.Meshes[0];
+			mainMesh.AutoTransformEnable = true;
+			//Movemos el mesh un poco para arriba. Porque sino choca con el piso todo el tiempo y no se puede mover.
+			mainMesh.move(0, 5, 0);
+
+            //Vamos a utilizar la camara en 3ra persona para que siga al objeto principal a medida que se mueve
+            camaraInterna = new TgcThirdPersonCamera(mainMesh.Position, 200, 300);
+            Camara = camaraInterna;
         }
 
         public override void Update()
         {
             PreUpdate();
+
+			//Obtenemos acceso al objeto que maneja input de mouse y teclado del framework
+			var input = Input;
+
+			//Declaramos un vector de movimiento inicializado en cero.
+			//El movimiento sobre el suelo es sobre el plano XZ.
+			//Sobre XZ nos movemos con las flechas del teclado o con las letas WASD.
+			var movement = new Vector3(0, 0, 0);
+
+			//Movernos de izquierda a derecha, sobre el eje X.
+			if (input.keyDown(Key.Left) || input.keyDown(Key.A))
+			{
+				movement.X = 1;
+			}
+			else if (input.keyDown(Key.Right) || input.keyDown(Key.D))
+			{
+				movement.X = -1;
+			}
+
+			//Movernos adelante y atras, sobre el eje Z.
+			if (input.keyDown(Key.Up) || input.keyDown(Key.W))
+			{
+				movement.Z = -1;
+			}
+			else if (input.keyDown(Key.Down) || input.keyDown(Key.S))
+			{
+				movement.Z = 1;
+			}
+
+			//Guardar posicion original antes de cambiarla
+			var originalPos = mainMesh.Position;
+
+			//Multiplicar movimiento por velocidad y elapsedTime
+			movement *= MOVEMENT_SPEED * ElapsedTime;
+			mainMesh.move(movement);
+
+			//Chequear si el objeto principal en su nueva posici√≥n choca con alguno de los objetos de la escena.
+			//Si es as√≠, entonces volvemos a la posici√≥n original.
+			//Cada TgcMesh tiene un objeto llamado BoundingBox. El BoundingBox es una caja 3D que representa al objeto
+			//de forma simplificada (sin tener en cuenta toda la complejidad interna del modelo).
+			//Este BoundingBox se utiliza para chequear si dos objetos colisionan entre s√≠.
+			//El framework posee la clase TgcCollisionUtils con muchos algoritmos de colisi√≥n de distintos tipos de objetos.
+			//Por ejemplo chequear si dos cajas colisionan entre s√≠, o dos esferas, o esfera con caja, etc.
+			var collisionFound = false;
+
+			foreach (var mesh in scene.Meshes)
+			{
+				//Los dos BoundingBox que vamos a testear
+				var mainMeshBoundingBox = mainMesh.BoundingBox;
+				var sceneMeshBoundingBox = mesh.BoundingBox;
+
+				//Ejecutar algoritmo de detecci√≥n de colisiones
+				var collisionResult = TgcCollisionUtils.classifyBoxBox(mainMeshBoundingBox, sceneMeshBoundingBox);
+
+				//Hubo colisi√≥n con un objeto. Guardar resultado y abortar loop.
+				if (collisionResult != TgcCollisionUtils.BoxBoxResult.Afuera)
+				{
+					collisionFound = true;
+					break;
+				}
+			}
+
+			//Si hubo alguna colisi√≥n, entonces restaurar la posici√≥n original del mesh
+			if (collisionFound)
+			{
+				mainMesh.Position = originalPos;
+			}
+
+			//Hacer que la camara en 3ra persona se ajuste a la nueva posicion del objeto
+			camaraInterna.Target = mainMesh.Position;
         }
 
         public override void Render()
         {
             PreRender();
 
-            //En cada cuadro de render rotamos la caja con cierta velocidad (en radianes)
-            //Siempre tenemos que multiplicar las velocidades por el elapsedTime.
-            //De esta forma la velocidad de rotacion es independiente de la potencia del CPU.
-            //Sino en computadoras con CPU m·s r·pido la caja girarÌa mas r·pido que en computadoras mas lentas.
-            box.rotateY(ROTATION_SPEED * ElapsedTime);
+            //Dibujar objeto principal
+            //Siempre primero hacer todos los calculos de logica e input y luego al final dibujar todo (ciclo update-render)
+            mainMesh.render();
 
-            //Tambien aplicamos una traslaciÛn en Y. Hacemos que la caja se mueva en forma intermitente en el
-            //intervalo [0, 3] de Y. Cuando llega a uno de los lÌmites del intervalo invertimos la direcciÛn
-            //del movimiento.
-            //Tambien tenemos que multiplicar la velocidad por el elapsedTime
-            box.move(0, MOVEMENT_SPEED * currentMoveDir * ElapsedTime, 0);
-            if (FastMath.Abs(box.Position.Y) > 3f)
-            {
-                currentMoveDir *= -1;
-            }
+            //Dibujamos la escena
+            scene.renderAll();
 
-            box.render();
+			//En este ejemplo a modo de debug vamos a dibujar los BoundingBox de todos los objetos.
+			//Asi puede verse como se efect√∫a el testeo de colisiones.
+			mainMesh.BoundingBox.render();
+			foreach (var mesh in scene.Meshes)
+			{
+				mesh.BoundingBox.render();
+			}
 
             PostRender();
         }
 
         public override void Dispose()
         {
-            box.dispose();
+            scene.disposeAll();
+            mainMesh.dispose();
         }
     }
 }
