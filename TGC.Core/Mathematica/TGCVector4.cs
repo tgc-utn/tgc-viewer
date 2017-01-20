973a25d76fd5cc431412cee3277b2dcd5917c21a
René Juan Rico Mendoza <rricomendoza@cideso.mil.ar>
2017-01-20 13:46:24 -0300

    Se crearon TGCVector4 y TGCQuaternion.

---
+++ TGC.Core/Mathematica/TGCVector4.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+using Microsoft.DirectX;
+
+namespace TGC.Core.Mathematica
+{
+    public class TGCVector4
+    {
+
+        #region Old TGCVectorUtils
+
+        /// <summary>
+        ///     convierte un color base(255,255,255,255) a un Vector4(1f,1f,1f,1f).
+        /// </summary>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        public static Vector4 ColorToVector4(Color color)
+        {
+            return Vector4.Normalize(new Vector4(color.R, color.G, color.B, color.A));
+        }
+
+        #endregion
+    }
+}
