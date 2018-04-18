144c715b71f02f889781b10ded4ff5609d51c70c
René Juan Rico Mendoza <rejurime@gmail.com>
2018-04-17 23:28:52 -0300

    Update version 2.3.3

--- TGC.Viewer/Properties/AssemblyInfo.cs
+++ TGC.Viewer/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 //      Revisión
 //
 
-[assembly: AssemblyVersion("2.3.1.0")]
-[assembly: AssemblyFileVersion("2.3.1.0")]
+[assembly: AssemblyVersion("2.3.3.0")]
+[assembly: AssemblyFileVersion("2.3.3.0")]
